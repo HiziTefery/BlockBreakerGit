4b283e2ea61ce8d45dff34179b908925ecfcced1
Unknown <hizitefery@gmail.com>
2018-04-12 07:20:34 +0200

    Initial

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+	// Use this for initialization
+	void OnCollisionEnter2D(Collision2D collision) {
+		print ("Collision");
+	}
+	public void OnTriggerEnter2D(Collider2D other) {
+		print("Trigger");
+		levelManager.LoadLevel("Win Screen");
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
